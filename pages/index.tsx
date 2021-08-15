204a6ad97dc64d06f382fc2ab218199c3b72cdc8
dukesx <afzaalbci@ymail.com>
2021-08-15 22:51:37 +0500

    Update
    Updated and Cleaned

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,3 @@
-import Wrapper from 'components/global/wrapper';
-import {
-  withAuthUserTokenSSR,
-  useAuthUser,
-  withAuthUser,
-} from 'next-firebase-auth';
-import { FunctionComponent } from 'react';
-
-interface TokenProps {
-  token?: String;
-}
-const Abc: FunctionComponent<TokenProps> = ({ token }) => {
-  return (
-    <Wrapper tokener={token}>
-      <p>Hello World</p>
-    </Wrapper>
-  );
+export default () => {
+  return <p>Hello World </p>;
 };
-
-export default withAuthUser()(Abc);
-
-export const getServerSideProps = withAuthUserTokenSSR()(
-  async ({ AuthUser: AuthUser }) => {
-    const token = await AuthUser.getIdToken();
-    return {
-      props: {
-        token: token,
-      },
-    };
-  }
-);
