6b90e975c528fc570e04f5a3d5654d34e35f1a66
dukesx <afzaalbci@ymail.com>
2021-08-16 23:02:30 +0500

    added Antd Dark Mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-const Index: React.FC = () => <p>Hello World</p>;
+import { Button } from "antd";
+import useDarkMode from "use-dark-mode";
+import { useThemeSwitcher } from "react-css-theme-switcher";
+
+const Index: React.FC = () => {
+  const dark = useDarkMode();
+  const { switcher, themes, currentTheme, status } = useThemeSwitcher();
+
+  return (
+    <>
+      <p>
+        Hello Current:{currentTheme} useDark: {dark.value ? "yes" : "false"}
+      </p>
+      <Button
+        type="primary"
+        onClick={() => {
+          dark.toggle();
+          switcher({ theme: dark.value ? "dark" : "light" });
+        }}
+      >
+        hello world
+      </Button>
+    </>
+  );
+};
 
 export default Index;
