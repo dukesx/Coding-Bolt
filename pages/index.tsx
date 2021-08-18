1e345d540eb71bb7f5f41e4a652f20ac791db4c1
dukesx <afzaalbci@ymail.com>
2021-08-18 17:49:04 +0500

    Check latest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
 import { Button } from "antd";
 import useDarkMode from "use-dark-mode";
 import Wrapper from "components/wrapper";
+import Head from "next/head";
 
 const Index: React.FC = () => {
   const dark = useDarkMode();
   //   const { switcher, themes, currentTheme, status } = useThemeSwitcher();
 
   return (
-    <Wrapper>
-      <div>
-        <p>
-          <b>Hello World</b>
-        </p>
-        useDark: {dark.value ? "yes" : "false"}
-      </div>
-    </Wrapper>
+    <>
+      <Head>
+        <title>Welcome to Coding Bolt</title>
+      </Head>
+      <Wrapper>
+        <div>
+          <p>
+            <b>Hello World</b>
+          </p>
+          useDark: {dark.value ? "yes" : "false"}
+        </div>
+      </Wrapper>
+    </>
   );
 };
 
