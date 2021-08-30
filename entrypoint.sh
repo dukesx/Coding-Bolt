03e05fff91211c658b367709edb6f4ba2865d22c
dukesx <afzaalbci@ymail.com>
2021-08-30 22:47:25 +0500

    asdsd

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 echo "Checking Vars"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_COOKIE_C#$CURRENT#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_COOKIE_P#$PREVIOUS#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#COOK#$CURRENT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#POOK#$PREVIOUS#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#imgkit#$NEXT_PUBLIC_IMAGE_CDN_PATH#g"
 
 echo "Starting Coding Bolt App"
