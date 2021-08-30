268a4fece26c09cc184cbe83a37da4652515af3d
dukesx <afzaalbci@ymail.com>
2021-08-30 23:15:38 +0500

    aa

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 echo "Checking Vars"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#COOK#$CURRENT#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#POOK#$PREVIOUS#g"
 find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#imgkit#$NEXT_PUBLIC_IMAGE_CDN_PATH#g"
 
 echo "Starting Coding Bolt App"
