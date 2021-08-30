179622aa383c01657c6f246a97cb80b0f0cee795
dukesx <afzaalbci@ymail.com>
2021-08-30 15:42:56 +0500

    Fixed Issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#APP_!/bin/sh
 
 echo "Checking Vars"
 
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#FIREBASE_PRIVATE_KEY#$FIREBASE_PRIVATE_KEY#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#COOKIE_SECRET_CURRENT#$COOKIE_SECRET_CURRENT#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#COOKIE_SECRET_PREVIOUS#$COOKIE_SECRET_PREVIOUS#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_IMAGE_CDN_PATH#$NEXT_PUBLIC_IMAGE_CDN_PATH#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#BUCKET_KEY#$BUCKET_KEY#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#BUCKET_URL#$BUCKET_URL#g"
-find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#NEXT_PUBLIC_API_KEY#$NEXT_PUBLIC_API_KEY#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_FIREBASE_PRIVATE_KEY#APP_$FIREBASE_PRIVATE_KEY#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_COOKIE_SECRET_CURRENT#APP_$COOKIE_SECRET_CURRENT#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_COOKIE_SECRET_PREVIOUS#APP_$COOKIE_SECRET_PREVIOUS#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_IMAGE_CDN_PATH#APP_$NEXT_PUBLIC_IMAGE_CDN_PATH#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_BUCKET_KEY#APP_$BUCKET_KEY#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_BUCKET_URL#APP_$BUCKET_URL#g"
+find /app/.next \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#APP_NEXT_PUBLIC_API_KEY#APP_$NEXT_PUBLIC_API_KEY#g"
 
 
 echo "Starting Coding Bolt App"
