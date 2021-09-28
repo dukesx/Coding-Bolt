e0d17c447cde7d56af283743f6039b67c70d9252
dukesx <afzaalbci@ymail.com>
2021-09-28 20:07:06 +0500

    better

--- components/global/wrapper.tsx
+++ components/global/wrapper.tsx
@@ -1,23 +1,10 @@
-import { Container, Button } from "@mantine/core";
+import { Container, Button, Text } from "@mantine/core";
 import Nav from "./nav";
-// import { signIn, signOut, useSession } from "next-auth/client";
 import Script from "next/script";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { supabase } from "lib/supabaseClient";
 import Flicking from "@egjs/react-flicking";
-import { Arrow } from "@egjs/flicking-plugins";
-// import {
-//   AngularjsOriginalIcon,
-//   SvelteOriginalIcon,
-//   ReactOriginalIcon,
-//   VuejsOriginalIcon,
-//   DartOriginalIcon,
-// } from "react-devicons";
-import AngularjsOrignalIcon from "react-devicons/angularjs/original";
-import VuejsOrignalIcon from "react-devicons/vuejs/original";
 import ReactOrignalIcon from "react-devicons/react/original";
-import SvelteOrignalIcon from "react-devicons/svelte/original";
-import DartOrignalIcon from "react-devicons/dart/original";
 import AngularjsOriginalIcon from "react-devicons/angularjs/original";
 import DartOriginalIcon from "react-devicons/dart/original";
 import VuejsOriginalIcon from "react-devicons/vuejs/original";
@@ -26,6 +13,7 @@ import SvelteOriginalIcon from "react-devicons/svelte/original";
 const Wrapper = (props: any) => {
   const [session, setSession] = useState(null);
   const sessioner = supabase.auth.session();
+  const myRefer = useRef();
   supabase.auth.onAuthStateChange((event, session) => {
     setSession(session);
     fetch("/api/auth", {
@@ -51,50 +39,177 @@ const Wrapper = (props: any) => {
       <Nav session={session} />
       <div className="bg-gray-100 p-2 dark:bg-transparent">
         <Flicking
-          // firstPanelSize="200px"
-          // hideBeforeInit={true}
-          circular={true}
+          hideBeforeInit={true}
           align="center"
-          gap={15}
           horizontal
-          // resizeOnContentsReady
-          // adaptive
+          circular={true}
+          defaultIndex={0}
         >
-          <Button
-            leftIcon={<ReactOrignalIcon />}
-            className="dark:bg-[#25262b] dark:font-bold dark:text-white"
-            variant="white"
-          >
-            React
-          </Button>
-          <Button
-            leftIcon={<AngularjsOriginalIcon />}
-            className="dark:bg-[#25262b] dark:font-bold dark:text-white"
-            variant="white"
-          >
-            Angular
-          </Button>
-          <Button
-            leftIcon={<DartOriginalIcon />}
-            className="dark:bg-[#25262b] dark:font-bold dark:text-white"
-            variant="white"
-          >
-            Dart
-          </Button>
-          <Button
-            leftIcon={<VuejsOriginalIcon />}
-            className="dark:bg-[#25262b] dark:font-bold dark:text-white"
-            variant="white"
-          >
-            Vue
-          </Button>
-          <Button
-            leftIcon={<SvelteOriginalIcon />}
-            className="dark:bg-[#25262b] dark:font-bold dark:text-white"
-            variant="white"
-          >
-            Svelte
-          </Button>
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<AngularjsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Angular
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<DartOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Dart
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<VuejsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Vue
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<SvelteOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Svelte
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<AngularjsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Angular
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<DartOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Dart
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<VuejsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Vue
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<SvelteOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Svelte
+            </Button>
+          </div>
+
+          <div className="mx-2">
+            <Button
+              leftIcon={<ReactOrignalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              React
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<AngularjsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Angular
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<DartOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Dart
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<VuejsOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Vue
+            </Button>
+          </div>
+          <div className="mx-2">
+            <Button
+              leftIcon={<SvelteOriginalIcon />}
+              className="dark:bg-[#25262b] dark:font-bold dark:text-white"
+              variant="white"
+            >
+              Svelte
+            </Button>
+          </div>
         </Flicking>
       </div>
       <Container className="xs:p-0 xxs:p-0 pt-0 px-0" size={1660}>
