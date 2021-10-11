2b11f3805dda5b957203952b9bc6d322d4e59fd7
dukesx <afzaalbci@ymail.com>
2021-10-11 15:46:10 +0500

    ci(Next Config): :sparkles: Adds Build Size Calculator
    Adds Relative CI's Build Stats

--- next.config.js
+++ next.config.js
@@ -1,3 +1,19 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+const withBundleStats = require("next-plugin-bundle-stats")({
+  outDir: "../artifacts",
+  stats: {
+    context: "./src", // optional, will improve readability of the paths
+    assets: true,
+    entrypoints: true,
+    chunks: true,
+    modules: true,
+  },
+  json: true,
+});
+
+module.exports = withBundleStats({
   reactStrictMode: true,
-}
+});
